6fbc7b6d530107a9110e547336e1ede4241e8c3c
GUSTAVO BATISTA CALHERANI <25253922@educ123.sp.senai.br>
2026-03-05 10:52:18 -0300

    criação de novas páginas, e ajustes

--- Script/Script.js
+++ Script/Script.js
@@ -50,6 +50,7 @@ function PRETO(){
     const Footer = document.getElementById("resist")
     const Style = document.getElementById("widen")
     const Shrek = document.getElementById("BORACO")
+    const Burro = document.getElementById("Burro")
     if(click % 2 == 0){
     if(IMG){
     Slide.classList.toggle("Modo_Claro")
@@ -63,6 +64,7 @@ function PRETO(){
     Style.href = "./css/css_White.css"
     Footer.href = "./css/footer_White.css"
     Shrek.style.filter = "invert(100%)"
+    Burro.style.filter = "invert(100%)"
     click++
     }
     else{
@@ -73,7 +75,8 @@ function PRETO(){
     Icon1.classList.toggle("col-section_Black")
     Icon1.classList.remove("col-section")
     Slide.style.backgroundImage = "url(./Img/Pingu_Branco.gif)"
-    Shrek.style.filter = "invert(100%)" 
+    Shrek.style.filter = "invert(100%)"
+    Burro.style.filter = "invert(100%)" 
     Header.href = "./css/header_White.css"
     Style.href = "./css/css_White.css"
     Footer.href = "./css/footer_White.css" 
@@ -90,6 +93,7 @@ function PRETO(){
     Icon1.classList.remove("col-section_Black")
     IMG.style.filter = "invert(0%)"
     Shrek.style.filter = "invert(0%)"
+    Burro.style.filter = "invert(0%)"
     Header.href = "./css/header.css"
     Style.href = "./css/style.css"
     Footer.href = "./css/footer.css"
@@ -104,6 +108,7 @@ function PRETO(){
     Icon1.classList.remove("col-section_Black")
     Slide.style.backgroundImage = "url(./Img/Pingu.gif)"
     Shrek.style.filter = "invert(0%)"
+    Burro.style.filter = "invert(0%)"
     Header.href = "./css/header.css"
     Style.href = "./css/style.css"
     Footer.href = "./css/footer.css"
@@ -111,4 +116,94 @@ function PRETO(){
     }
     }
 
+}
+
+function PretoGeral(){
+    const Header = document.getElementById("message");
+    const Footer = document.getElementById("resist");
+    const Style = document.getElementById("widen");
+    const Shrek = document.getElementById("BORACO");
+    const Burro = document.getElementById("Burro");
+    if(click % 2 == 0){
+    Header.href = "../css/header_White.css";
+    Style.href = "../css/css_White.css";
+    Footer.href = "../css/footer_White.css";
+    Shrek.style.filter = "invert(100%)";
+    Burro.style.filter = "invert(100%)";
+    click++;
+    }
+    else{
+    Shrek.style.filter = "invert(0%)";
+    Burro.style.filter = "invert(0%)";
+    Header.href = "../css/header.css";
+    Style.href = "../css/style.css";
+    Footer.href = "../css/footer.css";
+    click--; 
+    }
+}
+
+function PretoServico(){
+    const Header = document.getElementById("message");
+    const Footer = document.getElementById("resist");
+    const Style = document.getElementById("Serv");
+    const Shrek = document.getElementById("BORACO");
+    const Burro = document.getElementById("Burro");
+    if(click % 2 == 0){
+    Header.href = "../css/header_White.css";
+    Style.href = "../css/styles.css";
+    Footer.href = "../css/footer_White.css";
+    Shrek.style.filter = "invert(100%)";
+    Burro.style.filter = "invert(100%)";
+    click++;
+    }
+    else{
+    Style.href = "../css/styles_Servicos.css";
+    Shrek.style.filter = "invert(0%)";
+    Burro.style.filter = "invert(0%)";
+    Header.href = "../css/header.css";
+    Footer.href = "../css/footer.css";
+    click--; 
+    }
+}
+
+function COISA(){
+    const Shrek = document.getElementById("BORACO")
+    Shrek.style.transition = "5s"
+    Shrek.style.opacity = "0%"
+    setTimeout(() => {
+    Shrek.src = "../img/shrek2.jpg"
+       Shrek.style.opacity = "100%"
+       Shrek.style.height = "80vh"
+    }, 5000);
+    setTimeout(() => {
+       Shrek.style.transition = "1s" 
+       Shrek.style.transform = "scale(3)" 
+    }, 7000);
+        setTimeout(() => {
+       Shrek.style.transform = "scale(1)" 
+    }, 8000);
+        setTimeout(() => {
+       Shrek.style.transition = "1s" 
+       Shrek.style.transform = "scale(3)" 
+    }, 9000);
+        setTimeout(() => {
+       Shrek.style.transform = "scale(1)" 
+    }, 10000);
+        setTimeout(() => {
+       Shrek.style.transition = "1s" 
+       Shrek.style.transform = "scale(3)" 
+    }, 11000);
+        setTimeout(() => {
+       Shrek.style.transform = "scale(1)" 
+    }, 12000);
+        setTimeout(() => {
+       Shrek.style.transition = "1s" 
+       Shrek.style.transform = "scale(3)" 
+    }, 13000);
+        setTimeout(() => {
+       Shrek.style.transform = "scale(1)" 
+    }, 14000);
+            setTimeout(() => {
+       Shrek.src = "../img/FixBackground.png" 
+    }, 15000);
 }
